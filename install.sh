1d8bbc9ce877382a3a829700e79aaaa4a33bbdb0
Thomas Kilian <thomaskilian@macbooki7.home>
2012-03-14 19:49:37 +0100

    Initially the .vim directory might be missing. Lets create it.

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ if [ -e $HOME/.vim/plugin/MKD.vim ]; then
   echo "DONE.."
 fi
 
+if [ ! -d $HOME/.vim ]; then
+  echo "Creating $HOME/.vim ..."
+  mkdir $HOME/.vim
+fi
+
+if [ ! -d $HOME/.vim/plugin ]; then
+  echo "Creating $HOME/.vim/plugin ..."
+  mkdir $HOME/.vim/plugin
+fi
+
 echo "Copying plugin/* to $HOME/.vim/plugin ... "
 cp -R plugin/* $HOME/.vim/plugin
 echo "DONE.."
